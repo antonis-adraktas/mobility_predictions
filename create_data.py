61386de1d95c4d099818df24307cfd71aa4ed0fb
antonis-adraktas <antonis.adraktas@gmail.com>
2021-11-25 11:57:43 +0200

    refactor parameters to be class variables

--- create_data.py
+++ create_data.py
@@ -1,10 +1,20 @@
 import numpy as np
 import pandas as pd
 
-column_names = ["Current state", "Next state"]
-
 
 class GenerateData:
+    column_names = ["Current state", "Next state"]
+    states = ["S1", "S4", "S5", "S6", "S7", "S8", "S9"]
+    transition_probs = {
+        "S1": [0, 0.8, 0.1, 0.05, 0.05, 0, 0],
+        "S8": [0.9, 0, 0, 0, 0.1, 0, 0],
+        "S7": [0.8, 0.2, 0, 0, 0, 0, 0],
+        "S4": [0, 0, 0.2, 0.5, 0, 0.15, 0.15],
+        "S5": [0.5, 0.2, 0, 0, 0.2, 0.1, 0],
+        "S6": [0, 0.6, 0, 0, 0, 0.25, 0.15],
+        "S9": [0.55, 0.2, 0.2, 0, 0.05, 0, 0]
+    }
+
     def __init__(self, num_samples: int):
         """This function will initialize a GenerateDara object, which contains a pandas Dataframe with the data created.
         This is synthetic data for  mobility prediction based on the states and probabilities presented on paper
@@ -12,41 +22,31 @@ class GenerateData:
         num_samples defines the number of samples to be produced for each state.
         """
         self.num_samples = num_samples
-        
-        states = ["S1", "S4", "S5", "S6", "S7", "S8", "S9"]
+
         current_state = []
         next_state = []
-        transition_probs = {
-            "S1": [0, 0.8, 0.1, 0.05, 0.05, 0, 0],
-            "S8": [0.9, 0, 0, 0, 0.1, 0, 0],
-            "S7": [0.8, 0.2, 0, 0, 0, 0, 0],
-            "S4": [0, 0, 0.2, 0.5, 0, 0.15, 0.15],
-            "S5": [0.5, 0.2, 0, 0, 0.2, 0.1, 0],
-            "S6": [0, 0.6, 0, 0, 0, 0.25, 0.15],
-            "S9": [0.55, 0.2, 0.2, 0, 0.05, 0, 0]
-        }
 
-        for i in states:
+        for i in self.states:
             for k in range(num_samples):
                 current_state.append(i)
-                if i in transition_probs.keys():
-                    next_state.append(np.random.choice(states, p=transition_probs[i]))
+                if i in self.transition_probs.keys():
+                    next_state.append(np.random.choice(self.states, p=self.transition_probs[i]))
                 else:
                     next_state.append(np.nan)
 
         variables = [current_state, next_state]
         self.df = pd.DataFrame(variables).transpose()
-        self.df.columns = column_names
+        self.df.columns = self.column_names
 
-        column_names.append("Evidence")
-        self.df[column_names[2]] = None
+        self.column_names.append("Evidence")
+        self.df[self.column_names[2]] = None
 
     def add_evidence(self, state1: str, state2: str, evidence: list, probability: list):
         """This function adds evidence data on the given transition from state1 to state2
         based on the evidence and probability lists provided"""
         for i in range(len(self.df)):
-            if self.df[column_names[0]][i] == state1 and self.df[column_names[1]][i] == state2:
-                self.df[column_names[2]][i] = np.random.choice(evidence, p=probability)
+            if self.df[self.column_names[0]][i] == state1 and self.df[self.column_names[1]][i] == state2:
+                self.df[self.column_names[2]][i] = np.random.choice(evidence, p=probability)
 
 
 data = GenerateData(1000)
@@ -56,7 +56,7 @@ data.add_evidence("S4", "S5", ["E1/E2", "E1", None], [0.4, 0.4, 0.2])
 data.add_evidence("S4", "S8", ["E3", None], [0.8, 0.2])
 data.add_evidence("S4", "S9", ["E2", None], [0.6, 0.4])
 
-
+column_names = data.column_names
 unique_states = data.df["Current state"].unique()
 for i in unique_states:
     print("Groupby state: " + i)
